3794d29fd50271f0e5835a5262b43bdb5ab95773
Alex de Roos <100049383+Alex-de-Roos@users.noreply.github.com>
2022-10-26 20:49:39 +0200

    navbar animation type 1 with arrow stuff dont know if likeylikey yet tho

--- js/main.js
+++ js/main.js
@@ -10,6 +10,8 @@ function menuIconAnimation(el) {
 
 function showMenu() {
     console.log('showMenu');
+    let navbar = document.getElementsByClassName('nav')[0];
+    navbar.classList.toggle('hideNav');
 }
 
 //
