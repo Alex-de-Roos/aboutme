5716f7c4eac8c4e3b2a03df5f0c6ccd0aa0bf21f
Alex de Roos <100049383+Alex-de-Roos@users.noreply.github.com>
2022-10-26 17:20:27 +0200

    switch stations wip

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
 //dark / light theme switching //////////////////////////////////////////////////////////////////////////////////
-function themeSwitch() {
-
-    let nightIcon = document.getElementById('night');
-    let dayIcon = document.getElementById('day');
-    let switcher = document.getElementById('switcher');
-    let state = switcher.getAttribute.theme;
-
-    dayIcon.style.display.block;
-    nightIcon.style.display.none;
-    // document.getElementById('switcher').style.flexDirection.reverse;
+function switchTheme() {
     console.log("themeswitch");
 }
