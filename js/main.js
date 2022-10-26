7a583e2db643ce02f086e1248b340a337758029b
Alex de Roos <100049383+Alex-de-Roos@users.noreply.github.com>
2022-10-26 10:57:26 +0200

    theme switch wip

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 //dark / light theme switching //////////////////////////////////////////////////////////////////////////////////
+function themeSwitch() {
+
+    let nightIcon = document.getElementById('night');
+    let dayIcon = document.getElementById('day');
+    let switcher = document.getElementById('switcher');
+    let state = switcher.getAttribute.theme;
+
+    dayIcon.style.display.block;
+    nightIcon.style.display.none;
+    // document.getElementById('switcher').style.flexDirection.reverse;
+    console.log("themeswitch");
+}
