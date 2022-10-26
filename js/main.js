c4ef503a0665b207997247720ac324d6fa785f0b
Alex de Roos <100049383+Alex-de-Roos@users.noreply.github.com>
2022-10-26 20:18:55 +0200

    menu button animation working!!

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//menu button 
+function clickMenu(el) {
+    menuIconAnimation(el);
+    showMenu();
+}
+
+function menuIconAnimation(el) {
+    el.classList.toggle('change');
+}
+
+function showMenu() {
+    console.log('showMenu');
+}
+
+//
